ce337cce4fb38af2fce74b8221000f92f3d18830
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-29 22:55:07 -0300

    Criando classe main

---
+++ campo-minado/src/br/com/ksg/cm/Main.java
@@ -0,0 +1,15 @@
+package br.com.ksg.cm;
+
+import br.com.ksg.cm.model.Tabuleiro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		
+		System.out.println(tabuleiro.toString());
+
+	}
+
+}
